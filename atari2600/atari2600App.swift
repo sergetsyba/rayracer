a11e2376472e4bea0aa1da2be96b42c5f2136c42
Serge Tsyba <tsyba@me.com>
2023-05-20 10:46:17 +0300

    Created an empty Xcode project.

---
+++ atari2600/atari2600App.swift
@@ -0,0 +1,17 @@
+//
+//  atari2600App.swift
+//  atari2600
+//
+//  Created by Serge Tsyba on 20.5.2023.
+//
+
+import SwiftUI
+
+@main
+struct atari2600App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
