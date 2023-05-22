10b1ee8327846cd13e7e14c4754d88c2667db9d5
Serge Tsyba <tsyba@me.com>
2023-05-22 03:26:46 +0300

    Added base type definitions for console and its CPU.

---
+++ Atari2600Kit/CPU.swift
@@ -0,0 +1,37 @@
+//
+//  CPU.swift
+//  Atari2600Kit
+//
+//  Created by Serge Tsyba on 22.5.2023.
+//
+
+public struct MOS6507 {
+	public typealias Word = UInt8
+	public typealias Address = UInt16
+	
+	private(set) public var accumulator: Word
+	private(set) public var X: Word
+	private(set) public var Y: Word
+	private(set) public var status: Status
+	
+	private(set) public var stackPointer: Word
+	private(set) public var programCounter: Address
+}
+
+public extension MOS6507 {
+	struct Status: OptionSet {
+		public var rawValue: UInt8
+		
+		public init(rawValue: UInt8) {
+			self.rawValue = rawValue
+		}
+		
+		static let carry = Status(rawValue: 1 << 0)
+		static let zero = Status(rawValue: 1 << 1)
+		static let interrupt = Status(rawValue: 1 << 2)
+		static let decimal = Status(rawValue: 1 << 3)
+		static let `break` = Status(rawValue: 1 << 4)
+		static let overflow = Status(rawValue: 1 << 6)
+		static let negative = Status(rawValue: 1 << 7)
+	}
+}
---
+++ Atari2600Kit/Console.swift
@@ -0,0 +1,10 @@
+//
+//  Console.swift
+//  Atari2600Kit
+//
+//  Created by Serge Tsyba on 22.5.2023.
+//
+
+public struct Atari2600 {
+	private(set) public var cpu: MOS6507
+}
