10a351e018b3862ddfff3237be43e2ed7676a8b6
Serge Tsyba <tsyba@me.com>
2023-06-16 16:21:27 +0300

    Added ability to set breakpoints in assembly view.

--- Atari2600/Debugger/AssemblyViewController.swift
+++ Atari2600/Debugger/AssemblyViewController.swift
@@ -12,6 +12,7 @@ import Atari2600Kit
 class AssemblyViewController: NSViewController {
 	@IBOutlet private var tableView: NSTableView!
 	
+	private let eventSubject = PassthroughSubject<Event, Never>()
 	private let console: Atari2600 = .current
 	private var cancellables: Set<AnyCancellable> = []
 	
@@ -35,6 +36,11 @@ class AssemblyViewController: NSViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		self.tableView.registerNibs([
+			"AssemblyAddressCellView": .addressCell,
+			"AssemblyDataCellView": .dataCell
+		])
+		
 		self.updateTableColumnWidths()
 		self.updateSinks()
 	}
@@ -50,10 +56,30 @@ class AssemblyViewController: NSViewController {
 
 
 // MARK: -
+// MARK: Event management
+extension AssemblyViewController {
+	public enum Event {
+		case breakpointSet(MOS6507.Address)
+		case breakpointUnset(MOS6507.Address)
+	}
+	
+	public var events: some Publisher<Event, Never> {
+		return self.eventSubject
+	}
+	
+	@objc func breakpointToggled(_ sender: BreakpointToggle) {
+		let event: Event = sender.isOn
+		? .breakpointSet(sender.tag)
+		: .breakpointUnset(sender.tag)
+		
+		self.eventSubject.send(event)
+	}
+}
+
+
+// MARK: -
 // MARK: UI updates
 private extension AssemblyViewController {
-	static let font: NSFont = .monospacedSystemFont(ofSize: 11.0, weight: .regular)
-	
 	func updateSinks() {
 		self.console.$cartridge
 			.sink() { [unowned self] in
@@ -76,11 +102,11 @@ private extension AssemblyViewController {
 		let sizes = ["$0000", "adc", "($a4),Y"]
 			.map() {
 				return $0.size(withAttributes: [
-					.font: Self.font
+					.font: NSFont.monospacedRegular
 				])
 			}
 		
-		self.tableView.tableColumns[0].width = sizes[0].width * 1.5
+		self.tableView.tableColumns[0].width = sizes[0].width * 1.75
 		self.tableView.tableColumns[1].width = sizes[1].width
 		self.tableView.tableColumns[2].width = sizes[2].width
 	}
@@ -112,41 +138,59 @@ extension AssemblyViewController: NSTableViewDelegate {
 	}
 	
 	func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-		guard let cellView = tableView.makeView(withIdentifier: .cell, owner: nil) as? AssemblyTableCellView,
-			  let (address, instruction) = self.program?[row] else {
+		guard let (address, instruction) = self.program?[row] else {
 			return nil
 		}
 		
 		switch tableColumn {
 		case tableView.tableColumns[0]:
-			cellView.label.stringValue = String(format: "$%04x", address)
+			let view = tableView.makeView(withIdentifier: .addressCell, owner: nil) as! AssemblyAddressCellView
+			view.toggle.title = String(address: address)
+			
+			view.toggle.tag = address
+			view.toggle.target = self
+			view.toggle.action = #selector(self.breakpointToggled(_:))
+			
+			return view
+			
 		case tableView.tableColumns[1]:
-			cellView.label.stringValue = "\(instruction.mnemonic)"
+			let view = tableView.makeView(withIdentifier: .dataCell, owner: nil) as! AssemblyDataCellView
+			view.label.stringValue = String(mnemonic: instruction.mnemonic)
+			return view
+			
 		case tableView.tableColumns[2]:
-			cellView.label.stringValue = String(format: instruction.mode.formatPattern, instruction.operand)
+			let view = tableView.makeView(withIdentifier: .dataCell, owner: nil) as! AssemblyDataCellView
+			view.label.stringValue = String(addressingMode: instruction.mode, operand: instruction.operand)
+			return view
+			
 		default:
 			return nil
 		}
-		
-		cellView.label.font = Self.font
-		return cellView
 	}
 }
 
 private extension NSUserInterfaceItemIdentifier {
-	static let cell = NSUserInterfaceItemIdentifier("AssemblyTableCellView")
-}
-
-
-// MARK: -
-// MARK: Table view cells
-class AssemblyTableCellView: NSTableCellView {
-	@IBOutlet var label: NSTextField!
+	static let addressCell = NSUserInterfaceItemIdentifier("AssemblyAddressCell")
+	static let dataCell = NSUserInterfaceItemIdentifier("AssemblyDataCell")
 }
 
 
 // MARK: -
 // MARK: Data formatting
+private extension String {
+	init(address: Int) {
+		self = .init(format: "$%04x", address)
+	}
+	
+	init(mnemonic: MOS6507.Mnemonic) {
+		self = "\(mnemonic)"
+	}
+	
+	init(addressingMode mode: MOS6507.AddressingMode, operand: Int) {
+		self = .init(format: mode.formatPattern, operand)
+	}
+}
+
 private extension MOS6507.AddressingMode {
 	var formatPattern: String {
 		switch self {
@@ -198,6 +242,13 @@ private extension NSScrollView {
 }
 
 private extension NSTableView {
+	func registerNibs(_ nibs: [NSNib.Name: NSUserInterfaceItemIdentifier], bundle: Bundle = .main) {
+		for (name, id) in nibs {
+			let nib = NSNib(nibNamed: name, bundle: bundle)
+			self.register(nib, forIdentifier: id)
+		}
+	}
+	
 	func ensureRowVisible(_ row: Int) {
 		let scrollView = self.enclosingScrollView!
 		let rowRect = self.rect(ofRow: row)
