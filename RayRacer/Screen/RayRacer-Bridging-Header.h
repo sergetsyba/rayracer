4f7fc91bb609c6e0314c6940f13462ec42d28080
Serge Tsyba <serge.tsyba@icloud.com>
2024-09-01 12:14:17 +0300

    Moved NTSC palette definition into a C header to be available to both Swift and Metal.

---
+++ RayRacer/Screen/RayRacer-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  RayRacer-Bridging-Header.h
+//  RayRacer
+//
+//  Created by Serge Tsyba on 1.9.2024.
+//
+
+#ifndef RayRacer_Bridging_Header_h
+#define RayRacer_Bridging_Header_h
+
+#include "ntsc_palette.h"
+
+#endif /* RayRacer_Bridging_Header_h */
