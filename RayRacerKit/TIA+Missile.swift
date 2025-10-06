f7b9e82322767f9a6196cc80bbbef7994987fe4f
Serge Tsyba <serge.tsyba@icloud.com>
2025-10-06 09:49:00 +0300

    Added support for RESMPx register functionality to TIA.

--- RayRacerKit/TIA+Missile.swift
+++ RayRacerKit/TIA+Missile.swift
@@ -12,14 +12,19 @@ extension TIA {
 		}
 		public var motion: Int = 0
 		
-		public var enabled: Bool = false
+		public var isEnabled: Bool = false
+		public var isResetToPlayer: Bool = false
+		
 		public var copies: Int = 1
 		public var size: Int = 1
 		
 		var needsDrawing: Bool {
-			return Self.sections[self.copies][self.position / 8]
-			&& self.enabled
-			&& self.position < self.size
+			if !self.isEnabled || self.isResetToPlayer {
+				return false
+			} else {
+				return Self.sections[self.copies][self.position / 8]
+				&& self.position < self.size
+			}
 		}
 	}
 }
