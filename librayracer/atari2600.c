79cc337712f7329b8f0f69d3399f8fab1ba9a40d
Serge Tsyba <serge.tsyba@icloud.com>
2025-12-13 16:30:33 +0200

    Added Atari 2600 simaltion to librayracer.

--- librayracer/atari2600.c
+++ librayracer/atari2600.c
@@ -6,3 +6,107 @@
 //
 
 #include "atari2600.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static int read_bus(void *bus, int address) {
+	racer_atari2600 *console = (racer_atari2600 *)bus;
+	
+	if (address & (1<<12)) {
+		return console->cartridge[address & 0x0fff];
+	} else if ((address & 0x280) == 0x280) {
+		return racer_mcs6532_read(console->riot, address & 0x1f);
+	} else if ((address & 0x80) == 0x80) {
+		return console->riot->memory[address & 0x7f];
+	} else {
+		racer_tia_read(*console->tia, address & 0x3f);
+	}
+	
+	printf("read_bus: unknown address $%04x\n", address);
+	return 0;
+}
+
+static void write_bus(void *bus, int address, int data) {
+	racer_atari2600 *console = (racer_atari2600 *)bus;
+	
+	if ((address & 0xf000) == 0xf000) {
+		printf("write_bus: ignoring write at rom address $%04x.", address);
+	} else if ((address & 0x280) == 0x280) {
+		racer_mcs6532_write(console->riot, address & 0x1f, data);
+	} else if ((address & 0x80) == 0x80) {
+		console->riot->memory[address & 0x7f] = data;
+	} else {
+		racer_tia_write(console->tia, address & 0x3f, data);
+	}
+}
+
+static int read_port_a(void *peripheral) {
+	return 0;
+}
+
+static int read_port_b(void *peripheral) {
+	return 0;
+}
+
+static void write_port_a(void *peripheral, int data) {
+	
+}
+
+static void write_port_b(void *peripheral, int data) {
+	
+}
+
+static void no_operation(void) {
+	
+}
+
+racer_atari2600 *racer_atari2600_create(void) {
+	racer_atari2600 *console = (racer_atari2600 *)malloc(sizeof(racer_atari2600));
+	
+	// create and wire MPU
+	console->mpu = (racer_mcs6507 *)malloc(sizeof(racer_mcs6507));
+	console->mpu->bus = console;
+	console->mpu->read_bus = read_bus;
+	console->mpu->write_bus = write_bus;
+	
+	// create and wire RIOT
+	console->riot = (racer_mcs6532 *)malloc(sizeof(racer_mcs6532));
+	console->riot->interrupt = no_operation;
+	
+	memcpy(console->riot->peripherals, (void *[]){
+		console,
+		console
+	}, sizeof(console->riot->peripherals));
+	memcpy(console->riot->read_port, (int (*[])(void *)){
+		read_port_a,
+		read_port_b
+	}, sizeof(console->riot->read_port));
+	memcpy(console->riot->write_port, (void (*[])(void *, int)){
+		write_port_a,
+		write_port_b
+	}, sizeof(console->riot->write_port));
+	
+	// create and wire TIA
+	console->tia = (racer_tia *)malloc(sizeof(racer_tia));
+	console->tia->is_ready = &console->mpu->is_ready;
+	racer_tia_init();
+	
+	return console;
+}
+
+void racer_atari2600_reset(racer_atari2600 *console) {
+	racer_mcs6507_reset(console->mpu);
+	racer_mcs6532_reset(console->riot);
+	racer_tia_reset(console->tia);
+}
+
+void racer_atari2600_advance_clock(racer_atari2600 *console) {
+	racer_mcs6507_advance_clock(console->mpu);
+	racer_mcs6532_advance_clock(console->riot);
+	
+	racer_tia_advance_clock(console->tia);
+	racer_tia_advance_clock(console->tia);
+	racer_tia_advance_clock(console->tia);
+}
--- librayracer/atari2600.h
+++ librayracer/atari2600.h
@@ -8,6 +8,22 @@
 #ifndef atari2600_h
 #define atari2600_h
 
-#include <stdio.h>
+#include <stdint.h>
+
+#include "mcs6507.h"
+#include "mcs6532.h"
+#include "tia.h"
+
+typedef struct {
+	racer_mcs6507 *mpu;
+	racer_mcs6532 *riot;
+	racer_tia *tia;
+	
+	uint8_t *cartridge;
+} racer_atari2600;
+
+racer_atari2600 *racer_atari2600_create(void);
+void racer_atari2600_reset(racer_atari2600 *console);
+void racer_atari2600_advance_clock(racer_atari2600 *console);
 
 #endif /* atari2600_h */
